aebdd013d71f9ca5c822c1f11fc2d47d01d8577f
chen-qingyu <chen_qingyu@qq.com>
2026-03-23 23:32:36 +0800

    init: create project

---
+++ xmake.lua
@@ -0,0 +1,14 @@
+set_project("flux")
+set_version("0.1.0")
+set_license("MIT")
+set_languages("cxx20")
+set_encodings("utf-8")
+
+add_rules("mode.debug", "mode.release")
+add_requires("entt", "pugixml")
+
+target("flux")
+    set_kind("binary")
+    set_rundir(".")
+    add_packages("entt", "pugixml")
+    add_files("src/*.cpp")

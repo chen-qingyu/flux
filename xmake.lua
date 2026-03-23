1d9baa8e39e6a9f4c6dab6c376159444a01486b3
chen-qingyu <chen_qingyu@qq.com>
2026-03-23 23:57:30 +0800

    build: add deps spdlog, magic_enum

--- xmake.lua
+++ xmake.lua
@@ -5,10 +5,10 @@ set_languages("cxx20")
 set_encodings("utf-8")
 
 add_rules("mode.debug", "mode.release")
-add_requires("entt", "pugixml")
+add_requires("entt", "pugixml", "spdlog", "magic_enum")
 
 target("flux")
     set_kind("binary")
     set_rundir(".")
-    add_packages("entt", "pugixml")
+    add_packages("entt", "pugixml", "spdlog", "magic_enum")
     add_files("src/*.cpp")
